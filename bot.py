1dee11bb1e3352610f6d581685d9e093a9d040aa
Coldhe <cubeice775@gmail.com>
2020-05-25 03:36:08 +0700

    cool

---
+++ bot.py
@@ -0,0 +1,56 @@
+import discord
+from discord.ext import commands
+import asyncio
+import random
+import datetime
+import embedlinks
+import conv
+import aidairo
+
+def read_token():
+    with open("token.txt","r") as f:
+        lines = f.readlines()
+        return lines[0].strip()
+
+token = read_token()
+
+client = commands.Bot(command_prefix='m!')
+
+
+@client.command()
+async def hello(ctx):
+        await ctx.send(f"hello~ {ctx.author.name}.")
+
+@client.command()
+async def colorpage(ctx):
+    chosen_image = random.choice(embedlinks.images)
+
+    embed = discord.Embed(timestamp=datetime.datetime.utcnow())
+    embed.set_image(url=chosen_image)
+    embed.set_footer(text=f"Requested by : {ctx.author.name}")
+
+    await ctx.send(embed=embed)
+
+@client.command()
+async def aidairo(ctx):
+    chosen_image = random.choice(embedlinks.illust)
+
+    embed = discord.Embed(timestamp=datetime.datetime.utcnow())
+    embed.set_image(url=chosen_image)
+    embed.set_footer(text=f"Requested by : {ctx.author.name}")
+
+    await ctx.send(embed=embed)
+
+@client.command()
+async def meichan(ctx):
+        await ctx.send(random.choice(conv.reponses))
+
+
+
+
+
+
+
+
+
+client.run(token)
---
+++ conv.py
@@ -0,0 +1,18 @@
+reponses = ["Oh you're in an artblock? Me too!! We can beat the block together! <:meisurprise:708916774304743434>",
+            "I am the school mystery number four, nice to meet you.",
+            "That's all there is to it.",
+            "Aoi-chan told me an interesting rumor the other day.",
+            "Surely the real world isn't that bad.",
+            "'I have zero intention on dying' ...I miss her.",
+            "hm?",
+            "A research found that learning and practising art strongly correlates with a higher achievement in reading and maths.",
+            "Painting the Mona Lisa’s lips took Leonardo da Vinci 12 years! Only the lips!",
+            "Roman Statues were made with detachable heads. One head could be taken off and replaced by yet another one.",
+            "There can be more than one solution to a problem is what arts teach us. Look until you find a solution.",
+            "Everyone is born creative. Some just need more practice to find their creativity.",
+            "In 1565, the first pencil was invented in England.",
+            "Andy Brown, an English artist, stitched together 1000 used tea bags, to create a portrait of Queen Elizabeth II.",
+            "Hey hey, want to draw together with me?",
+            ""
+
+            ]
